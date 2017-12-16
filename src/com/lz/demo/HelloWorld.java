a7ad24ccdbe83aec1d704176236d8022ae19dad7
lizheng18 <lizheng18@meituan.com>
2017-12-17 00:02:54 +0800

    commit

---
+++ src/com/lz/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lz.demo;
+
+/**
+ * Created by lizheng18 2017/12/16 下午11:53
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("helloworld");
+    }
+}
